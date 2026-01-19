03ae0320ce97fda83f382bea3456a6995f9b133f
Crossoufire <47953962+Crossoufire@users.noreply.github.com>
2026-01-19 15:33:47 +0100

    update base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     site: "https://crossoufire.github.io",
+    base: "/my-portfolio",
     vite: {
         plugins: [tailwindcss()],
     },
